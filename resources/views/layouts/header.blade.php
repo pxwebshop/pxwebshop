a683a201cb6056406b7903928bec36b790f752a4
dangquy95php@gmail.com <dangquy95php@gmail.com>
2022-09-06 20:52:04 +0700

    update

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,104 +1,52 @@
 <header class="c-header">
-    <div class="c-header__top pc-only">
-        <div class="l-container">
-            <p class="c-header__nameCompany">PX WebShop</p>
-            <ul class="c-header__address">
-                <li class="c-header__address__item">
-                    <span><i class="fa-solid fa-envelope"></i> pxwebshop@gmail.com</span>
+    <div class="l-container">
+        <figure>
+            <img src="https://placehold.jp/150x80.png" alt="">
+        </figure>
+        <nav class="c-header__nav">
+            <ul class="c-header__list">
+                <li class="c-header__item">
+                    <a class="c-header__link" href="#">Trang chủ</a>
                 </li>
-                <li class="c-header__address__item">
-                    <a href="tel: 020 28266650"><i class="fa-solid fa-phone"></i> 020 28266650</a>
+                <li class="c-header__item c-header__item__services">
+                    <span class="c-header__link" href="#">Dịch Vụ<i class="fa-solid fa-caret-down"></i></span>
+
+                    <ul class="c-header__child">
+                        <li class="c-header__child__item">
+                            <a href="/web-design">Thiết kế website</a>
+                        </li>
+                        <li class="c-header__child__item">
+                            <a href="/build-software">Viết chương trình khi cần thiết</a>
+                        </li>
+                        <li class="c-header__child__item">
+                            <a href="/logo-design">Thiết kế logo</a>
+                        </li>
+                        <li class="c-header__child__item">
+                            <a href="/web-app">Viết ứng dụng web</a>
+                        </li>
+                        <li class="c-header__child__item">
+                            <a href="/seo-services">Dịch vụ SEO</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="c-header__item">
+                    <a class="c-header__link" href="#">Bài viết</a>
+                </li>
+                <li class="c-header__item">
+                    <a class="c-header__link" href="#">Về chúng tôi</a>
+                </li>
+                <li class="c-header__item">
+                    <i class="fa-solid fa-magnifying-glass is-searchMenu"></i>
+                    <div class="c-header__search">
+                        <form action="#" class="c-header__form search" role="search" method="get">
+                            <input type="search" name="s" placeholder="Tìm kiếm" value="" id="search-form-63146b0878fe2">
+                            <button class="c-header__btnSearch" type="submit">
+                                <i class="fa-solid fa-magnifying-glass"></i>
+                            </button>
+                        </form>
+                    </div>
                 </li>
             </ul>
-        </div>
+        </nav>
     </div>
-    <div class="c-header__gnav sp-only">
-        <div class="c-header__logoSp">
-            <a href="#">
-                <img src="https://www.takabou.jp/wp-content/themes/No1_theme01/img/common/logo.svg" alt="高橋防水">
-            </a>
-        </div>
-
-        <div class="c-header__btn">
-            <div class="c-header__tel__btn">
-                <span class="tel_link"><a href="tel:020 28266650">020 28266650</a></span>
-            </div>
-
-            <div class="c-header__gnav__btn" id="gnav_open">
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-        </div>
-    </div>
-    <nav class="c-header__gnav__child sp-only" id="gnav">
-        <ul class="c-header__gnav__menu">
-            <li class="c-header__gnav__item">
-                <a href="/index.php/"><span>ໜ້າຫຼັກ</span></a>
-            </li>
-            <li class="c-header__gnav__item">
-                <a href="/repository-interface"><span>ຕົວຢ່າງເວັບໄຊ</span></a>
-            </li>
-            <li class="c-header__gnav__item">
-                <a class="c-header__gnav__parent" href="#">
-                    <span>ບໍລິການ</span>
-                    <i class="fa-solid fa-plus"></i>
-                    <i class="fa-solid fa-minus"></i>
-                </a>
-                <ul class="c-header__gnav__menu__sub">
-                    <li class="c-header__gnav__item"><i class="fa-solid fa-angle-double-right"></i>
-                        <a href="/web-design"><span>ອອກແບບເວັບໄຊ</span></a>
-                    </li>
-                    <li class="c-header__gnav__item"><i class="fa-solid fa-angle-double-right"></i>
-                        <a href="/index.php/xay-dung-phan-mem"><span>ຂຽນໂປຣແກຣມຕາມຄວາມຕ້ອງການ</span></a>
-                    </li>
-                    <li class="c-header__gnav__item"><i class="fa-solid fa-angle-double-right"></i>
-                        <a href="/index.php/xay-dung-phan-mem"><span>ອອກແບບເຄືອຂ່າຍ</span></a>
-                    </li>
-                    <li class="c-header__gnav__item"><i class="fa-solid fa-angle-double-right"></i>
-                        <a href="/index.php/thiet-ke-logo"><span>ອອກແບບໂລໂກ</span></a>
-                    </li>
-                    <li class="c-header__gnav__item"><i class="fa-solid fa-angle-double-right"></i>
-                        <a href="/web-app">ຂຽນ Web App</a>
-                    </li>
-                    <li class="c-header__gnav__item"><i class="fa-solid fa-angle-double-right"></i>
-                        <a href="/seo-services">ບໍລິການ SEO </a>
-                    </li>
-                </ul>
-            </li>
-            <li class="c-header__gnav__item">
-                <a href="/news">
-                    <span>ຂ່າວສານ</span>
-                </a>
-            </li>
-            <li class="c-header__gnav__item">
-                <a class="c-header__gnav__parent" href="#">
-                    <span>ກຽ່ວກັບເຮົາ</span>
-                    <i class="fa-solid fa-plus"></i>
-                    <i class="fa-solid fa-minus"></i>
-                </a>
-                <ul class="c-header__gnav__menu__sub">
-                    <li class="c-header__gnav__item"><i class="fa-solid fa-angle-double-right"></i>
-                        <a href="/introduce"><span>ແນະນຳ</span></a>
-                    </li>
-                    <li class="c-header__gnav__item"><i class="fa-solid fa-angle-double-right"></i>
-                        <a href="/pay"><span>ຊຳລະເງິນ</span></a>
-                    </li>
-                    <li class="c-header__gnav__item"><i class="fa-solid fa-angle-double-right"></i>
-                        <a href="/contact"><span>ຕິດຕໍ່</span></a>
-                    </li>
-                </ul>
-            </li>
-            <li class="c-header__itemSeach">
-                <div class="c-header__search">
-                    <form action="https://pxwebshop.com/index.php/" class="c-header__form search" role="search" method="get">
-                        <input type="search" name="s" placeholder="ໃສ່ຂໍ້ມູນທີ່ຕ້ອງການຄົ້ນຫາ" value="" id="search-form-630f7d25799fb">
-                        <button class="c-header__btnSearch" type="submit">
-                            <i class="fa-solid fa-magnifying-glass"></i>
-                        </button>
-                    </form>
-                </div>
-            </li>
-        </ul>
-    </nav>
 </header>
