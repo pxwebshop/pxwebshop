8990d34c46c1db573e05180fdef589c3ca90e40a
dangquy95php@gmail.com <dangquy95php@gmail.com>
2022-12-13 09:27:56 +0700

    add

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,65 @@
+<section class="p-home10 wow bounceInUp" data-wow-duration="0.7s" data-wow-delay="0.5s">
+    <div class="c-footer">
+        <div class="l-container">
+            <div class="c-footer__inner">
+                <div class="c-footer__item">
+                    <h5 class="c-footer__tlt">CÔNG TY TNHH TMĐT CÔNG NGHỆ PXWEBSHOP</h5>
+                    <address>
+                        <span>VP Miền Nam: Doing</span>
+                        <span>VP Miền Trung: Doing</span>
+                    </address>
+                    <figure class="c-footer__logo">
+                        <img src="https://placehold.jp/280x120.png" alt="" srcset="">
+                    </figure>
+                </div>
+                <div class="c-footer__item">
+                    <h5 class="c-footer__tlt">LIÊN HỆ</h5>
+                    <div class="c-footer__list">
+                        <li class="c-footer__item1">
+                            <span class="c-footer__item1__tlt">Email:</span>
+                            <span class="c-footer__item1__txt">pxwebshop@gmail.com</span>
+                        </li>
+                        <li class="c-footer__item1">
+                            <span class="c-footer__item1__tlt">Hotline/Zalo:</span>
+                            <span class="c-footer__item1__txt">
+                                <a href="tel:+4733378901">+47 333 78 901 </a>
+                            </span>
+                        </li>
+                        <li class="c-footer__item1">
+                            <span class="c-footer__item1__tlt">Tổng đài:</span>
+                            <span class="c-footer__item1__txt">
+                                <a href="tel:+4733378901">+47 333 78 901</a>
+                            </span>
+                        </li>
+                        <li class="c-footer__item1">
+                            <a class="c-footer__item1__link" href="#">
+                                <span class="c-footer__icon">
+                                    <i aria-hidden="true" class="fab fa-facebook"></i>
+                                </span>
+                                <span class="c-footer__icon__txt">Fanpage Pxwebshop</span>
+                            </a>
+                        </li>
+                    </div>
+                </div>
+                <div class="c-footer__item">
+                    <h5 class="c-footer__tlt">ĐIỀU KHOẢN CHÍNH SÁCH</h5>
+                    <ul class="c-footer__list1">
+                        <li class="c-footer__list1__item">
+                            <a href="">Chính Sách Bảo Mật</a>
+                        </li>
+                        <li class="c-footer__list1__item">
+                            <a href="">Điều Khoản Sử Dụng</a>
+                        </li>
+                        <li class="c-footer__list1__item">
+                            <a href="">Thông Tin Thanh Toán</a>
+                        </li>
+                        <li class="c-footer__list1__item">
+                            <a href="">Câu Hỏi Thường Gặp</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <p class="c-footer__copyright">Copyright © 2022 PX WebShop. All rights reserved.</p>
+        </div>
+    </div>
+</section>
