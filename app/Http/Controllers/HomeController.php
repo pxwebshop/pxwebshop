75d539ba76a2de92f128a81f6910eb8021d28dd3
dangquy95php@gmail.com <dangquy95php@gmail.com>
2023-01-09 13:07:32 +0700

    add

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -57,7 +57,7 @@ class HomeController extends Controller
           ]);
 
           if ($validator->fails()) {
-               return response()->json(['error' => $validator->errors()->all()], 422);
+               return response()->json(['error' => $validator->errors()], 422);
           }
           try {
                Order::create([
