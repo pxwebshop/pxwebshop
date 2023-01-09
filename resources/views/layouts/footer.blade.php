4b36e6992282630157aa1b6a711d62719be3b576
dangquy95php@gmail.com <dangquy95php@gmail.com>
2023-01-09 15:29:40 +0700

    add

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,16 +5,9 @@
 <div id="fb-customer-chat" class="fb-customerchat">
 </div>
 
-<!-- Messenger Plugin chat Code -->
-<div id="fb-root"></div>
-
-<!-- Your Plugin chat code -->
-<div id="fb-customer-chat" class="fb-customerchat">
-</div>
-
 <script>
   var chatbox = document.getElementById('fb-customer-chat');
-  chatbox.setAttribute("page_id", "105912482337654");
+  chatbox.setAttribute("page_id", "100919432505933");
   chatbox.setAttribute("attribution", "biz_inbox");
 </script>
 
