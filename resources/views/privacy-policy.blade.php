4fb7e2769776b2e670c2d8440d31c42b007596c0
Phonexay <phonexaynsv@gmail.com>
2023-01-21 23:21:57 +0700

    translate

--- resources/views/privacy-policy.blade.php
+++ resources/views/privacy-policy.blade.php
@@ -1,4 +1,4 @@
-@section('title','Chính sách bảo mật')
+@section('title','ນະໂຍບາຍຄວາມເປັນສ່ວນຕົວ')
 @section('description','Chính sách bảo mật luôn luôn quan trọng trong dịch vụ công ty lao smart tech')
 @section('keywords','privacy policy, thiết kế website, logo, banner, phần mềm')
 @extends('layouts.template')
@@ -9,13 +9,13 @@
     <div class="c-box13">
         <div class="c-box13__inner">
             <div class="l-container">
-                <h2 class="c-box13__tlt">Chính sách bảo mật</h2>
+                <h2 class="c-box13__tlt">ນະໂຍບາຍຄວາມເປັນສ່ວນຕົວ</h2>
                 <ul class="c-breadcrumb">
                     <li class="c-breadcrumb__item">
-                        <a class="c-breadcrumb__link" href="/#home">Trang chủ</a>
+                        <a class="c-breadcrumb__link" href="/#home">ໜ້າຫຼັກ</a>
                     </li>
                     <li class="c-breadcrumb__item">
-                        <span class="c-breadcrumb__txt">Bảo mật</span>
+                        <span class="c-breadcrumb__txt">ຄວາມປອດໄພ</span>
                     </li>
                 </ul>
             </div>
@@ -26,16 +26,15 @@
             <div class="c-box9">
                 <ul class="c-box9__list">
                     <li class="c-box9__item">
-                        <p class="c-box9__txt">
-                            Chúng tôi biết rằng sự riêng tư và bảo mật thông tin cá nhân rất quan trọng đối với người dùng, vì vậy chúng tôi cam kết nổ lực tối đa bảo vệ sự riêng tư của bạn.<br/>
-                            Xin vui lòng đọc bản chính sách bảo mật dưới đây để hiểu hơn những cam kết mà chúng tôi thực hiện nhằm tôn trọng và bảo vệ quyền lợi của bạn.<br/>
-                            Chính sách bảo mật mô tả cách thức chúng tôi thu thập và xử lý các thông tin cá nhân khi bạn sử dụng dịch vụ của Pxwebshop.com . Bạn đồng ý sử dụng dịch vụ của Pxwebshop.com ,có nghĩa là bạn hoàn toàn đồng ý với các nội dung chúng tôi nếu trong Chính sách bảo mật này.Chúng tôi có thể sửa đổi nội dung của chính sách bằng cách đăng một bản sửa đổi lên Pxwebshop.com , phiên bản sửa đổi có hiệu lực kể từ thời điểm đăng tải.                        
+                        <p class="c-box9__txt">ພວກເຮົາຮູ້ດີວ່າຄວາມເປັນສ່ວນຕົວ ແລະ ຄວາມປອດໄພຂໍ້ມູນສ່ວນຕົວຂອງລູກຄ້າ ຫຼື ຜູ້ໃຊ້ບໍລິການແມ່ນສໍາຄັນທີ່ສຸດ, ດັ່ງນັ້ນພວກເຮົາຈຶ່ງມຸ່ງມັ້ນທີ່ຈະເຮັດສຸດຄວາມສາມາດ ເພື່ອປົກປ້ອງຄວາມເປັນສ່ວນຕົວຂອງທ່ານ.
+                            ກະລຸນາອ່ານນະໂຍບາຍຄວາມເປັນສ່ວນຕົວຂ້າງລຸ່ມນີ້ ເພື່ອເຂົ້າໃຈຄໍາມັ້ນສັນຍາທີ່ພວກເຮົາດຳເນີນການເພື່ອເຄົາລົບ ແລະ ປົກປ້ອງສິດທິຂອງທ່ານ.<br/>
+                            ນະໂຍບາຍຄວາມເປັນສ່ວນຕົວນີ້ອະທິບາຍເຖິງວິທີການ ທີ່ພວກເຮົາເກັບກຳ ແລະ ຈັດການຂໍ້ມູນສ່ວນຕົວ ຂອງທ່ານເມື່ອໃຊ້ບໍລິການຂອງ laosmarttech.com. ເມື່ອທ່ານເຫັນດີທີ່ຈະໃຊ້ການບໍລິການຂອງ laosmarttech.com ຫມາຍຄວາມວ່າທ່ານຍອມຮັບກັບເນື້ອຫາທີ່ມີຢູ່ໃນນະໂຍບາຍຄວາມເປັນສ່ວນຕົວນີ້.                         
                         </p>
                     </li>
                     <li class="c-box9__item">
-                        <p class="c-box9__tlt">1. THÔNG TIN CHÚNG TÔI THU THẬP</p>
+                        <p class="c-box9__tlt">1. ຂໍ້ມູນທີ່ພວກເຮົາເກັບກໍາ</p>
                         <p class="c-box9__txt">
-    Để có thể sử dụng đầy đủ các tiện ích trên Pxwebshop.com , bạn cần phải đăng ký thành viên và cung cấp các thông tin cá nhân của mình. Chúng tôi có thể thu thập các thông tin sau về bạn:
+    Để có thể sử dụng đầy đủ các tiện ích trên laosmarttech.com , bạn cần phải đăng ký thành viên và cung cấp các thông tin cá nhân của mình. Chúng tôi có thể thu thập các thông tin sau về bạn:
     Tên công ty, cửa hàng, lĩnh vực kinh doanh
     Họ tên, tuổi, địa chỉ cư trú
     Email, số điện thoại di động, số điện thoại bàn
@@ -43,15 +42,15 @@
                         </p>
                     </li>
                     <li class="c-box9__item">
-                        <p class="c-box9__tlt">2. LƯU TRỮ THÔNG TIN CÁ NHÂN</p>
+                        <p class="c-box9__tlt">2. ການເກັບຮັກສາຂໍ້ມູນສ່ວນຕົວ</p>
                         <p class="c-box9__txt">
-                            Chúng tôi lưu trữ và xử lý thông tin cá nhân của bạn trên máy chủ, chúng tôi bảo vệ nó bằng các biện pháp bảo vệ vật lý, điện tử bao gồm: tường lửa, mã hóa dữ liệu và thủ tục áp dụng theo quy định của luật bảo mật thông tin. Pxwebshop.com thực thi kiểm soát truy cập vật lý vào các thông tin, và chúng tôi chỉ cho phép truy cập thông tin cá nhân đối với những nhân viên cần nó để hoàn thành trách nhiệm công việc của họ trong hệ thống Pxwebshop.com .
+                            Chúng tôi lưu trữ và xử lý thông tin cá nhân của bạn trên máy chủ, chúng tôi bảo vệ nó bằng các biện pháp bảo vệ vật lý, điện tử bao gồm: tường lửa, mã hóa dữ liệu và thủ tục áp dụng theo quy định của luật bảo mật thông tin. laosmarttech.com thực thi kiểm soát truy cập vật lý vào các thông tin, và chúng tôi chỉ cho phép truy cập thông tin cá nhân đối với những nhân viên cần nó để hoàn thành trách nhiệm công việc của họ trong hệ thống laosmarttech.com .
                         </p>
                     </li>
                     <li class="c-box9__item">
-                        <p class="c-box9__tlt">3. SỬ DỤNG THÔNG TIN CỦA BẠN</p>
+                        <p class="c-box9__tlt">3. ການ​ນໍາ​ໃຊ້​ຂໍ້​ມູນ​ຂອງ​ທ່ານ​</p>
                         <p class="c-box9__txt">
-                            Mục tiêu của Pxwebshop.com là mang lại cho doanh nghiệp, người dùng một hạ tầng dịch vụ phục vụ quảng bá và phát triển thương hiệu, mở rộng kinh doanh trên Internet một cách hiệu quả, tối ưu về chi phí và thời gian. Vì vậy, chúng tôi sử dụng thông tin cá nhân của bạn nhằm:
+                            Mục tiêu của laosmarttech.com là mang lại cho doanh nghiệp, người dùng một hạ tầng dịch vụ phục vụ quảng bá và phát triển thương hiệu, mở rộng kinh doanh trên Internet một cách hiệu quả, tối ưu về chi phí và thời gian. Vì vậy, chúng tôi sử dụng thông tin cá nhân của bạn nhằm:
     Cung cấp thông tin, các dịch vụ và hỗ trợ theo yêu cầu của bạn
     Gửi email thông báo các chương trình khuyến mãi, gia hạn dịch vụ, sự kiện tiêu biểu của chúng tôi
     Giải quyết các tranh chấp, các vấn đề phát sinh liên quan đến việc sử dụng website
@@ -61,35 +60,35 @@
                         </p>
                     </li>
                     <li class="c-box9__item">
-                        <p class="c-box9__tlt">4. TIẾT LỘ THÔNG TIN CỦA BẠN</p>
+                        <p class="c-box9__tlt">4. ເປີດ​ເຜີຍ​ຂໍ້​ມູນ​ຂອງ​ທ່ານ​</p>
                         <p class="c-box9__txt">
                             Chúng tôi cam kết không cung cấp thông tin cá nhân của bạn cho bất kỳ bên thứ ba nào. Tuy nhiên, chúng tôi có thể tiết lộ thông tin cá nhân của bạn trong một số trường hợp dưới đây:
     Chúng tôi được bạn đồng ý tiết lộ những thông tin này
     Bên thứ ba mà bạn ủy quyền hoặc cho phép có yêu cầu chúng tôi cung cấp thông tin cá nhân của bạn
     Theo yêu cầu pháp lý hay từ một cơ quan chính phủ hoặc nếu chúng tôi tin rằng hành động đó là cần thiết nhằm tuân theo các yêu cầu pháp lý hoặc chiếu theo luật pháp
     Bảo vệ quyền, lợi ích, tài sản, sự an toàn của một ai khác
-    Cho các bên thứ ba khác mà chúng tôi có liên doanh, liên kết để cung cấp các dịch vụ trên Pxwebshop.com hoặc các dịch vụ mới có liên quan đến Pxwebshop.com
+    Cho các bên thứ ba khác mà chúng tôi có liên doanh, liên kết để cung cấp các dịch vụ trên laosmarttech.com hoặc các dịch vụ mới có liên quan đến laosmarttech.com
     Ngoài những trường hợp nêu trên nhưng không giới hạn, chúng tôi sẽ không công bố thông tin cá nhân của bạn cho bên thứ ba nào khác trừ khi chúng tôi hoàn toàn tin rằng, sự công bố này là cần thiết nhằm ngăn chặn những thiệt hại vật chất hoặc tài chính do các yếu tố có dấu hiệu phạm pháp có thể gây ra.
                         </p>
                     </li>
                     <li class="c-box9__item">
-                        <p class="c-box9__tlt">5. ĐIỀU CHỈNH THÔNG TIN CÁ NHÂN</p>
+                        <p class="c-box9__tlt">5. ການປັບປ່ຽນຂໍ້ມູນສ່ວນຕົວ</p>
                         <p class="c-box9__txt">
     Bạn có thể truy cập, sửa chữa và cập nhật thông tin cá nhân trên website của mình bằng cách đăng nhập và sử dụng các tính năng quản trị. Khi cập nhật thông tin cá nhân của bạn, chúng tôi có thể yêu cầu bạn xác minh danh tính của mình trước khi chúng tôi xử lý yêu cầu của bạn
     Chúng tôi cũng có thể sẽ chủ động hoặc theo yêu cầu của bạn bổ sung, hiệu chỉnh hay tẩy xóa các dữ liệu thông tin cá nhân không chính xác, không đầy đủ hoặc không cập nhật khi bạn còn liên kết với hoạt động của chúng tôi.
                         </p>
                     </li>
                     <li class="c-box9__item">
-                        <p class="c-box9__tlt">6. GHI NHẬN THÔNG TIN TRÌNH DUYỆT (COOKIE)</p>
+                        <p class="c-box9__tlt">6. ການບັນ​ທຶກ​ຂໍ້​ມູນເຂົ້າຊົມ​ເວັບໄຊ (COOKIE)</p>
                         <p class="c-box9__txt">
-                            Cookie là một file dữ liệu được đặt trên đĩa cứng của bạn bởi máy chủ của Pxwebshop.com . Chúng tôi sử dụng Cookie để cá nhân hóa và nâng cao hiệu quả sử dụng thời gian trực tuyến của bạn.
-                            Cookie sẽ giúp chúng tôi nhận ra bạn nếu bạn truy cập vào nhiều trang trên trang web của chúng tôi trong cùng một phiên, do đó chúng tôi không cần phải hỏi mật khẩu của bạn trên mỗi trang. Sau khi bạn đăng xuất hoặc đóng trình duyệt, cookie này sẽ hết hạn và không còn có bất kỳ tác dụng nào. Bạn có thể chấp nhận hoặc từ chối dùng cookie. Hầu hết những Browser tự động chấp nhận cookie, nhưng bạn có thể thay đổi cài đặt để từ chối tất cả những cookie nếu bạn thích. Tuy nhiên, nếu bạn chọn từ chối cookie, điều đó có thể gây cản trở và ảnh hưởng đến một số dịch vụ và tính năng phụ thuộc vào cookie tạ Pxwebshop.com .
+                            Cookie là một file dữ liệu được đặt trên đĩa cứng của bạn bởi máy chủ của laosmarttech.com . Chúng tôi sử dụng Cookie để cá nhân hóa và nâng cao hiệu quả sử dụng thời gian trực tuyến của bạn.
+                            Cookie sẽ giúp chúng tôi nhận ra bạn nếu bạn truy cập vào nhiều trang trên trang web của chúng tôi trong cùng một phiên, do đó chúng tôi không cần phải hỏi mật khẩu của bạn trên mỗi trang. Sau khi bạn đăng xuất hoặc đóng trình duyệt, cookie này sẽ hết hạn và không còn có bất kỳ tác dụng nào. Bạn có thể chấp nhận hoặc từ chối dùng cookie. Hầu hết những Browser tự động chấp nhận cookie, nhưng bạn có thể thay đổi cài đặt để từ chối tất cả những cookie nếu bạn thích. Tuy nhiên, nếu bạn chọn từ chối cookie, điều đó có thể gây cản trở và ảnh hưởng đến một số dịch vụ và tính năng phụ thuộc vào cookie tạ laosmarttech.com .
                         </p>
                     </li>
                     <li class="c-box9__item">
-                        <p class="c-box9__tlt">7. LIÊN HỆ VÀ TRAO ĐỔI THÔNG TIN</p>
+                        <p class="c-box9__tlt">7. ຕິດຕໍ່ ແລະ ແລກປ່ຽນຂໍ້ມູນ</p>
                         <p class="c-box9__txt">
-                            Chúng tôi sẽ liên hệ và trao đổi thông tin thông qua địa chỉ E-mail mà bạn cung cấp khi đăng ký dịch vụ. Trường hợp có sự thay đổi hay phát hiện có sự sử dụng địa chỉ E-mail bất hợp pháp, bạn vui lòng thông báo ngay cho chúng tôi để có những biện pháp hỗ trợ kịp thời.
+                            ພວກເຮົາຈະຕິດຕໍ່ ແລະ ແລກປ່ຽນຂໍ້ມູນ ຜ່ານທີ່ຢູ່ອີເມວທີ່ທ່ານໃຫ້ໄວ້ເມື່ອເວລາລົງທະບຽນໃຊ້ບໍລິການ ໃນ​ກໍ​ລະ​ນີ​ທີ່​ມີ​ການ​ປ່ຽນ​ແປງ ​ຫຼື ​​ພົບເຫັນ​ການ​ນໍາ​ໃຊ້ທີ່​ຢູ່​ອີ​ເມວຂອງ​ທ່ານ​ທີ່​ບໍ່ຖືກຕ້ອງ ກະ​ລຸ​ນາ​ແຈ້ງຫາ​ພວກ​ເຮົາ​ທັນ​ທີເພື່ອຫາວິທີແກ້ໄຂໄດ້ຢ່າງ​​ທັນ​ເວ​ລາ​.
                         </p>
                     </li>
                 </ul>
